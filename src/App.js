8513cd7d998fa6a8a8475420d57829d3d14280bf
Jacob Handwerger <jstudyeth@gmail.com>
2023-06-02 19:04:11 -0400

    done except edge cases

--- src/App.js
+++ src/App.js
@@ -1,31 +1,73 @@
+import { useState } from "react";
+
 const App = () => {
+
+  const [ bill, setBill ] = useState(0);
+  const [ percentage, setPercentage ] = useState(0);
+  const [ people, setPeople ] = useState(0);
+  const [ total, setTotal ] = useState('0.00');
+  const [ tip, setTip ] = useState('0.00');
+
+  const handleBill = (e) => {
+    setBill(e.target.value);
+  }
+
+  const handleCustomTip = (e) => {
+    setPercentage(e.target.value);
+  }
+
+  const handlePeople = (e) => {
+    setPeople(e.target.value);
+  }
+
+  const reset = () => {
+    setTip(((+(percentage) / 100) * +(bill)) / +(people));
+    setTotal(+(bill) / +(people));
+  } 
+
   return (
     <>
       <img id="logo" src="logo.svg" alt="logo"/>
       <div id="card">
         <div className="left-card">
           <label>Bill</label>
-          <div className="input-container">
-            <input className="bill-people-input" placeholder="0"></input>
+          <div className="input-container-bill">
+            <input onChange={handleBill} className="bill-people-input" placeholder="0"></input>
           </div>
           <div className="tips">
-          <label>Select tip %</label>
+          <label>Select Tip %</label>
           <div className="tips-buttons">
-            <button>5%</button>
-            <button>10%</button>
-            <button>15%</button>
-            <button>25%</button>
-            <button>50%</button>
-            <input className="custom-input" placeholder="Custom"></input>
+            <button onClick={() => setPercentage(5)}>5%</button>
+            <button onClick={() => setPercentage(10)}>10%</button>
+            <button onClick={() => setPercentage(15)}>15%</button>
+            <button onClick={() => setPercentage(25)}>25%</button>
+            <button onClick={() => setPercentage(50)}>50%</button>
+            <input onChange={handleCustomTip} className="custom-input" placeholder="Custom"></input>
           </div>
           </div>
           <label>Number of People</label>
-          <div className="input-container">
-            <input className="bill-people-input" placeholder="0"></input>
+          <div className="input-container-people">
+            <input onChange={handlePeople} className="bill-people-input" placeholder="0"></input>
           </div>
         </div>
         <div className="right-card">
-          {/*  */}
+          <div className="result">
+            <div className="tip-total-amount">
+              <div className="dash-flex">
+                <h3>Tip Amount</h3>
+                <p>/ person</p>
+              </div>
+              <h1>${tip}</h1>
+            </div>
+            <div className="tip-total-amount">
+              <div className="dash-flex">
+                <h3>Total</h3>
+                <p>/ person</p>
+              </div>
+              <h1>${total}</h1>
+            </div>
+          </div>
+          <button onClick={reset} className="reset-button">Reset</button>
         </div>
       </div>
     </>
